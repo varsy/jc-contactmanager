1d8cbb0ecdc4fc0eb27e3be74db748b719db7e7b
Andrey Sizov <andrey.sizov@jetbrains.com>
2015-02-19 01:20:08 +0300

    added properties parsing

--- src/edu/javacourse/contact/dao/ContactDAODb.java
+++ src/edu/javacourse/contact/dao/ContactDAODb.java
@@ -3,7 +3,11 @@ package edu.javacourse.contact.dao;
 import edu.javacourse.contact.entity.Contact;
 import edu.javacourse.contact.filter.ContactFilter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.PropertyResourceBundle;
 
 /**
  * Project: CM2
@@ -13,6 +17,37 @@ import java.util.List;
  * Created with IntelliJ IDEA.
  */
 public class ContactDAODb implements ContactDAO {
+
+    private static PropertyResourceBundle confProps;
+    private static String jdbcUrl;
+    private static String jdbcUsername;
+    private static String jdbcPassword;
+
+    static {
+        try {
+            confProps = (PropertyResourceBundle) PropertyResourceBundle.getBundle("conf/ContactDAODb");
+            jdbcUrl = "jdbc:postgresql://" + confProps.getObject("contact.dao.db.host") + ":" + confProps.getObject("contact.dao.db.port") + "/" + confProps.getObject("contact.dao.db.database");
+            jdbcUsername = (String) confProps.getObject("contact.dao.db.user");
+            jdbcPassword = (String) confProps.getObject("contact.dao.db.pass");
+        } catch (Exception e) {
+            System.out.println("Can't parse properties file.");
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't find database driver.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(jdbcUrl,jdbcUsername,jdbcPassword);
+    }
+
     @Override
     public Long addContact(Contact contact) {
         return null;
